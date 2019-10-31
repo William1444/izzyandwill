63f506eca31be343a0c7d8db0f8d149af8ebb0c4
williamlacy2 <williamlacy2@gmail.com>
2019-10-31 21:56:58 +0000

    email client

--- config/index.js
+++ config/index.js
@@ -1,5 +1,9 @@
+const defaultSecrets = require('./../client_secret.json');
+
 module.exports = {
   mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017',
   databaseName: "izzyandwill",
-  adminKey: process.env.ADMIN_KEY || "test"
+  adminKey: process.env.ADMIN_KEY || "test",
+  sendgridApiKey: process.env.SENDGRID_API_KEY || defaultSecrets.sendgridApiKey,
+  fromEmail: 'orchardleigh.willandizzy@gmail.com'
 };
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const Room = require('./../models/room');
+const rsvpReply = require('./../services/rsvp-reply');
+const basicApiKey = require('./../middleware/basic-api-key');
 
 /* GET rooms */
 router.get('/', function (req, res, next) {
@@ -10,6 +12,16 @@ router.get('/', function (req, res, next) {
       key: req.query.key
     }))
     .catch(err => next(err))
+});
+
+router.get('/test', basicApiKey, function (req, res, next) {
+  rsvpReply.email({to: 'williamlacy2@gmail.com', firstName: 'Izzy', otherGuests: 'Pup', room: {room: 'Cheeky Bonita', price: '100.00'}})
+    .then(r => res.send(r))
+    .catch(e => next(e));
+});
+
+router.get('/test/html', basicApiKey, function (req, res, next) {
+  res.send(rsvpReply.html({firstName: 'Izzy', otherGuests: 'Pup', room: {room: 'Cheeky Bonita', price: '100.00'}}));
 });
 
 module.exports = router;
