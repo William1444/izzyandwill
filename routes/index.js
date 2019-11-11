91fbeb34d4bffce55c260a5421e5d65acbbec39c
williamlacy2 <williamlacy2@gmail.com>
2019-11-11 18:53:15 +0000

    add wedding view

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,32 @@ const { ensureAuthenticated } = require('./../config/auth');
 
 /* GET home page. */
 router.get('/', ensureAuthenticated, function (req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET home page. */
+router.get('/wedding', ensureAuthenticated, function (req, res) {
+  res.render('wedding');
+});
+
+/* GET home page. */
+router.get('/travel', ensureAuthenticated, function (req, res) {
+  res.render('travel');
+});
+
+/* GET home page. */
+router.get('/sleeping', ensureAuthenticated, function (req, res) {
+  res.render('sleeping');
+});
+
+/* GET home page. */
+router.get('/gifts', ensureAuthenticated, function (req, res) {
+  res.render('gifts');
+});
+
+/* GET home page. */
+router.get('/photos', ensureAuthenticated, function (req, res) {
+  res.render('photos');
 });
 
 module.exports = router;
