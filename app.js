e3c2a36cee464f39f2e9abce32c111b74af7f7da
williamlacy2 <williamlacy2@gmail.com>
2019-11-12 19:57:14 +0000

    make error handling on login better

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.use(flash());
 app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+  res.locals.authError = req.flash('error');
+  console.info(res.locals.authError);
   next();
 });
 
