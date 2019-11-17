d1bf3ad2f940a44a5dc207f512b4cccc832f1853
williamlacy2 <williamlacy2@gmail.com>
2019-11-17 21:15:22 +0000

    add photos to site

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated } = require('./../config/auth');
-const path = require('path');
+const {ensureAuthenticated} = require('./../config/auth');
 
-/* GET home page. */
 router.get('/', ensureAuthenticated, function (req, res) {
   res.render('index');
 });
 
-/* GET home page. */
 router.get('/wedding', ensureAuthenticated, function (req, res) {
   res.render('wedding');
 });
 
-/* GET home page. */
 router.get('/travel', ensureAuthenticated, function (req, res) {
   res.render('travel');
 });
 
-/* GET home page. */
 router.get('/sleeping', ensureAuthenticated, function (req, res) {
   res.render('sleeping');
 });
 
-/* GET home page. */
+
 router.get('/gifts', ensureAuthenticated, function (req, res) {
   res.render('gifts');
 });
 
-/* GET home page. */
+
 router.get('/photos', ensureAuthenticated, function (req, res) {
   res.render('photos');
 });
